592d7ec8e2a0daaa3492e8d5c309b0b0fe1f22be
GuldenKarakan <43781647+GuldenKarakan@users.noreply.github.com>
2019-10-22 00:05:03 +0300

    dusmanUretme

---
+++ kurtkuzu/Assets/Scripts/dusmanKontrol.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+public class dusmanKontrol : MonoBehaviour
+{
+    public NavMeshAgent takip_et;
+    public GameObject hedef_kup;
+    //public GameObject[] dogum_Noktasi;
+
+    void Start()
+    {
+
+        takip_et = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        takip_et.destination = hedef_kup.transform.position;
+        //Instantiate(gameObject, transform.position, transform.rotation);
+    }
+}
